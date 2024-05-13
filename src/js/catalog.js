6211a31a6c2335c73409500c8a42395750445271
Alizadekh <aykhanalizadekh@gmail.com>
2024-05-13 04:11:36 -0700

    Adding Responsive Design and Fix some bugs in Swipers

---
+++ src/js/catalog.js
@@ -0,0 +1,47 @@
+function open() {
+  document.querySelector(".downloadingImg").style.display = "none";
+  document.querySelector(".main").style.opacity = "1";
+}
+
+setTimeout(open, 2000);
+
+document.querySelector(".joinUsBtn").addEventListener("click", function () {
+  document.querySelector(".joinUsSection").style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
+
+document.addEventListener("click", function (e) {
+  const joinUsSection = document.querySelector(".joinUsSection");
+  const joinUs = document.querySelector(".joinUs");
+  const joinUsBtn = document.querySelector(".joinUsBtn");
+
+  if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
+    joinUsSection.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+});
+
+// Swiper
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: getSlidesPerView(),
+  direction: getDirection(),
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    resize: function () {
+      swiper.params.slidesPerView = getSlidesPerView();
+      swiper.update();
+    },
+  },
+});
+
+function getDirection() {
+  return "horizontal";
+}
+
+function getSlidesPerView() {
+  return window.innerWidth <= 600 ? 1 : 5;
+}

e69e63b0a3ab89f716d720b8b4ccc9c00cfa44eb
Alizadekh <aykhanalizadekh@gmail.com>
2024-05-17 23:13:38 -0400

    Add Modal Window

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,27 +1,60 @@
 function open() {
-    document.querySelector(".downloadingImg").style.display = "none";
-    document.querySelector(".main").style.opacity = "1";
+  document.querySelector(".downloadingImg").style.display = "none";
+  document.querySelector(".main").style.opacity = "1";
 }
 
 setTimeout(open, 2000);
 
 document.querySelector(".joinUsBtn").addEventListener("click", function () {
-    document.querySelector(".joinUsSection").style.display = "flex";
-    document.body.style.overflow = "hidden";
-})
+  document.querySelector(".joinUsSection").style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
 
 document.addEventListener("click", function (e) {
-    const joinUsSection = document.querySelector(".joinUsSection");    const joinUs = document.querySelector(".joinUs");
-const joinUsBtn = document.querySelector(".joinUsBtn")
+  const joinUsSection = document.querySelector(".joinUsSection");
+  const joinUs = document.querySelector(".joinUs");
+  const joinUsBtn = document.querySelector(".joinUsBtn");
 
-    // Check if the clicked element is not the joinUsBtn or its children
-    if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
-        joinUsSection.style.display = "none";
-        document.body.style.overflow = "auto";
-    }
+  // Check if the clicked element is not the joinUsBtn or its children
+  if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
+    joinUsSection.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
 });
 
 function open() {
-document.querySelector(".downloadingImg").style.display = "none";
-document.querySelector(".main").style.opacity = "1";
+  document.querySelector(".downloadingImg").style.display = "none";
+  document.querySelector(".main").style.opacity = "1";
 }
+
+// Modal Window Functionality
+
+const nameInput = document.querySelector("#name");
+const surNameInput = document.querySelector("#surname");
+const emailInput = document.querySelector("#email");
+const phoneInput = document.querySelector("#phone");
+const textInput = document.querySelector("#textarea");
+const send = document.querySelector(".sendBtn");
+
+const infoImg = document.querySelector(".warning-img");
+const infoMsg = document.querySelector(".warning-msg");
+const infoTitle = document.querySelector(".modal-title");
+
+function checkUser() {
+  if (
+    emailInput.value.length === 0 ||
+    phoneInput.value.length === 0 ||
+    textInput.value.length === 0
+  ) {
+    send.setAttribute("data-toggle", "modal");
+    send.setAttribute("data-target", "#exampleModal");
+  } else {
+    infoImg.innerHTML = `<img src="../img/contact/succes.jpg" alt="">`;
+    infoMsg.innerHTML = ` <br /> <p class="default-msg">Müraciətiniz göndərildi! Təşəkkür edirik!</p>`;
+    infoTitle.textContent = "GÖNDƏRİLDİ !";
+    send.setAttribute("data-toggle", "modal");
+    send.setAttribute("data-target", "#exampleModal");
+  }
+}
+
+send.addEventListener("click", checkUser);

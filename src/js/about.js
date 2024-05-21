ce089eed786cc1c83ac067447acad8f3d6487b13
Rufat239 <116900615+Rufat239@users.noreply.github.com>
2024-05-21 11:16:02 +0400

    about store page to firebase

--- src/js/about.js
+++ src/js/about.js
@@ -1,27 +1,54 @@
-function open() {
-    document.querySelector(".downloadingImg").style.display = "none";
-    document.querySelector(".main").style.opacity = "1";
-}
+// function open() {
+//   document.querySelector(".downloadingImg").style.display = "none";
+//   document.querySelector(".main").style.opacity = "1";
+// }
 
 setTimeout(open, 2000);
 
 document.querySelector(".joinUsBtn").addEventListener("click", function () {
-    document.querySelector(".joinUsSection").style.display = "flex";
-    document.body.style.overflow = "hidden";
-})
+  document.querySelector(".joinUsSection").style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
 
 document.addEventListener("click", function (e) {
-    const joinUsSection = document.querySelector(".joinUsSection");    const joinUs = document.querySelector(".joinUs");
-const joinUsBtn = document.querySelector(".joinUsBtn")
+  const joinUsSection = document.querySelector(".joinUsSection");
+  const joinUs = document.querySelector(".joinUs");
+  const joinUsBtn = document.querySelector(".joinUsBtn");
 
-    // Check if the clicked element is not the joinUsBtn or its children
-    if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
-        joinUsSection.style.display = "none";
-        document.body.style.overflow = "auto";
-    }
+  // Check if the clicked element is not the joinUsBtn or its children
+  if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
+    joinUsSection.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
 });
 
 function open() {
-document.querySelector(".downloadingImg").style.display = "none";
-document.querySelector(".main").style.opacity = "1";
+  document.querySelector(".downloadingImg").style.display = "none";
+  document.querySelector(".main").style.opacity = "1";
 }
+
+import db from "./firebase.mjs";
+import {
+  set,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const about = document.querySelector(".about");
+// const snapshot = push(ref(db, "/abouStoretData"));
+
+function showAboutStore(result) {
+  const data = result.val();
+  about.innerHTML += `
+    <div class="about-div1">
+    <h1>${data.title}</h1>
+    <p>${data.aboutDesc}</p>
+  </div>
+  <div class="img-div">
+    <img src="${data.aboutİmage}" alt="">
+  </div>`;
+}
+// }
+
+onValue(ref(db, "/abouStoretData"), showAboutStore);

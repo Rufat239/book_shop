d6a538537f31e50cb23ec1932f8b9d380ac8c0db
zehrashukurova <zehraashukurova@gmail.com>
2024-05-24 00:12:29 +0400

    add joinus firebase

--- src/js/about.js
+++ src/js/about.js
@@ -1,29 +1,68 @@
-import joinUs from "./joinUs.mjs";
-
-setTimeout(open, 2000);
-
-// document.querySelector(".joinUsBtn").addEventListener("click", function () {
-//   document.querySelector(".joinUsSection").style.display = "flex";
-//   document.body.style.overflow = "hidden";
-// });
-
-// document.addEventListener("click", function (e) {
-//   const joinUsSection = document.querySelector(".joinUsSection");
-//   const joinUs = document.querySelector(".joinUs");
-//   const joinUsBtn = document.querySelector(".joinUsBtn");
-
-//   // Check if the clicked element is not the joinUsBtn or its children
-//   if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
-//     joinUsSection.style.display = "none";
-//     document.body.style.overflow = "auto";
-//   }
-// });
-joinUs()
-
 function open() {
   document.querySelector(".downloadingImg").style.display = "none";
   document.querySelector(".main").style.opacity = "1";
 }
+setTimeout(open, 2000);
+
+
+document.querySelector(".joinUsBtn").addEventListener("click", function () {
+  document.querySelector(".joinUsSection").style.display = "flex";
+  document.body.style.overflow = "hidden";
+});
+
+document.addEventListener("click", function (e) {
+  const joinUsSection = document.querySelector(".joinUsSection");
+  const joinUs = document.querySelector(".joinUs");
+  const joinUsBtn = document.querySelector(".joinUsBtn");
+
+  // Check if the clicked element is not the joinUsBtn or its children
+  if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
+    joinUsSection.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+});
+
+
+
+function sendJoinUsToDatabase() {
+  document.querySelector(".joinSubmitBtn").addEventListener("click", function () {
+      const successAlert = document.querySelector(".alert-success");
+      successAlert.style.opacity = "1";
+      let name = document.querySelector(".nameInpJoinUs");
+      let email = document.querySelector(".emailInpJoinUs");
+      let message = document.querySelector(".messageInpJoinUs");
+      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (name.value.trim() !== "" && emailPattern.test(email.value)) {
+          // Show Bootstrap success alert
+          const info = {
+              email: email.value,
+              fullname: name.value,
+              message: message.value
+          }
+
+          
+
+    set(push(ref(db,'/joinus')),info)  
+          
+
+          successAlert.style.display = "block";
+          name.value = "";
+          email.value = "";
+          message.value = "";
+
+          // Hide the alert after a certain time
+          setTimeout(() => {
+              successAlert.style.opacity = "0";
+              document.querySelector(".joinUsSection").style.display = "none";
+          }, 3000);
+      }
+      else {
+          alert("Please write fullname and email!")
+      }
+  })
+}
+
+sendJoinUsToDatabase();
 
 import db from "./firebase.mjs";
 import {

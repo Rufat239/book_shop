f7989614950aa30d35cd5be8c39b30ffe9e21d25
zehrashukurova <zehraashukurova@gmail.com>
2024-05-23 23:05:43 +0400

    import joinus.mjs

--- src/js/bookpage.js
+++ src/js/bookpage.js
@@ -7,16 +7,16 @@ document.querySelector(".joinUsBtn").addEventListener("click", function () {
   document.querySelector(".joinUsSection").style.display = "flex";
   document.body.style.overflow = "hidden";
 });
-// document.addEventListener("click", function (e) {
-//   const joinUsSection = document.querySelector(".joinUsSection");
-//   const joinUs = document.querySelector(".joinUs");
-//   const joinUsBtn = document.querySelector(".joinUsBtn");
-//   // Check if the clicked element is not the joinUsBtn or its children
-//   if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
-//     joinUsSection.style.display = "none";
-//     document.body.style.overflow = "auto";
-//   }
-// });
+document.addEventListener("click", function (e) {
+  const joinUsSection = document.querySelector(".joinUsSection");
+  const joinUs = document.querySelector(".joinUs");
+  const joinUsBtn = document.querySelector(".joinUsBtn");
+  // Check if the clicked element is not the joinUsBtn or its children
+  if (!joinUsBtn.contains(e.target) && !joinUs.contains(e.target)) {
+    joinUsSection.style.display = "none";
+    document.body.style.overflow = "auto";
+  }
+});
 
 // Data almaq
 
